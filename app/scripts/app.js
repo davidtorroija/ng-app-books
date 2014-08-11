8ba17de9834040d1038290ffc75cf274215264cf
David Torroija <david.torroija@provedatos.com>
2014-08-11 17:37:48 -0500

    v

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,59 +9,17 @@
  * Main module of the application.
  */
 angular
-  .module('moviecityApp', [
+  .module('booksApp', [
     'ngAnimate',
     'ngRoute',
     'ui.bootstrap',
-    'Movies',
-    'Actors',
-    'Genres',
     'Books',
-    'localytics.directives',
-    'ui.directives'
+    // 'localytics.directives',
+    // 'ui.directives'
   ])
   //routes config!
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: 'scripts/main/views/main.html',
-        controller: 'MainCtrl',
-        resolve: {
-          movies: function($location,$route,movieModel) {
-            $location.path('/books');
-            var id = $route.current.params.idMovie;
-            return movieModel.get()
-            .catch(function() {
-              $location.path('/');
-            });
-          },
-        }
-      })
-      .when('/movies/edit/:idMovie', {
-        templateUrl: 'scripts/movies/views/movieEdit.html',
-        controller: 'MoviesEditCtrl',
-        resolve: {
-          movie: function($location,$route,movieModel) {
-            var id = $route.current.params.idMovie;
-            return movieModel.getById(id)
-            .catch(function() {
-              $location.path('/');
-            });
-          },
-          actors: function($location,$route, actorModel) {
-            return actorModel.get()
-            .catch(function() {
-              $location.path('/');
-            });
-          },
-          genres: function($location,$route, genreModel) {
-            return genreModel.get()
-            .catch(function() {
-              $location.path('/');
-            });
-          },
-        }
-      })
       .when('/movies/:idMovie', {
         templateUrl: 'scripts/movies/views/movie.html',
         controller: 'MoviesCtrl',
@@ -75,7 +33,7 @@ angular
           },
         }
       })
-      .when('/books', {
+      .when('/', {
         templateUrl: 'scripts/books/views/book.html',
         controller: 'BooksCtrl',
         resolve: {
--- app/scripts/movies/movieModel.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-angular.module('Movies').factory('movieModel', function(model){
-  return model('movies');
-});
