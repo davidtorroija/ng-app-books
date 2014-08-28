a52f82e7682ac92f16c23a1441fbe3419f787cd0
David Torroija <david.torroija@provedatos.com>
2014-08-28 08:51:08 -0500

    changed routes place

--- app/scripts/books/controllers/books.js
+++ app/scripts/books/controllers/books.js
@@ -69,5 +69,24 @@
     this.splice(newIndex, 0, this.splice(oldIndex, 1)[0]);
     return this;
   };
+})
+.config(function ($routeProvider) {
+  $routeProvider
+     .when('/books/:bookId', {
+      templateUrl: 'scripts/books/views/book.html',
+      controller: 'BooksCtrl',
+      resolve: {
+        book: function($location,$route,bookModel) {
+          var id = $route.current.params.bookId;
+          return bookModel.getById(id)
+          .catch(function() {
+            $location.path('/');
+          });
+        },
+      }
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
 });
 
