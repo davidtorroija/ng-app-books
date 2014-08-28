50827e7ce6a8a85d1ad1939f2f8bd5e399ca5057
David Torroija <david.torroija@provedatos.com>
2014-08-28 08:51:20 -0500

    changes routes places

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,23 +5,5 @@ angular
     'ngRoute',
     'ui.bootstrap',
     'Books',
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-       .when('/books/:bookId', {
-        templateUrl: 'scripts/books/views/book.html',
-        controller: 'BooksCtrl',
-        resolve: {
-          book: function($location,$route,bookModel) {
-            var id = $route.current.params.bookId;
-            return bookModel.getById(id)
-            .catch(function() {
-              $location.path('/');
-            });
-          },
-        }
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+  ]);
+
